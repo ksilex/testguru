d2cf8b4a91fdf5ebe4c66d1f81bed46517c16d05
ksilex <negative074@gmail.com>
2020-12-29 15:25:03 +0300

    task_3

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,11 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :questions
+  has_many :user_tests
+  has_many :users, through: :user_tests
+
+  def self.category_sort(category)
+    Test.joins(:category).where("categories.title = ?", category).
+    order(title: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :user_tests
+  has_many :tests, through: :user_tests
+
+  def tests_by_lvl(level)
+    tests.where(level: level)
+  end
 end
