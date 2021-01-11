320cf0a660c0c1ebc53ac13513543b8421581e4c
ksilex <negative074@gmail.com>
2021-01-11 15:55:27 +0300

    task 4

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,6 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: "User", foreign_key: "user_id"
   has_many :questions
   has_many :user_tests
   has_many :users, through: :user_tests
