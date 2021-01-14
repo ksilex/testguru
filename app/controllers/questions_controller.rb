37ba88994cfd45eba0ba224c4c72a3cec7d7f9c9
ksilex <negative074@gmail.com>
2021-01-14 17:09:43 +0300

    task 6 rest

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,38 @@
+class QuestionsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  before_action :associated_test, only: %i[index create new]
+
+  def index
+    render json: @test.questions
+  end
+
+  def show
+    render json: Question.find(params[:id])
+  end
+
+  def new
+    @question = @test.questions.new
+  end
+
+  def create
+    Question.create(question_params)
+  end
+
+  def destroy
+    Question.find(params[:id]).delete
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body, :test_id)
+  end
+
+  def associated_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def not_found
+    render plain: "Test not found"
+  end
+end
