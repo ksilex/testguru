e56aa5166f8a74a9c8c9aca7b5a98173d4dea51e
ksilex <negative074@gmail.com>
2021-01-23 21:45:32 +0300

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_22_085826) do
+ActiveRecord::Schema.define(version: 2021_01_23_184330) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body", null: false
@@ -39,10 +39,9 @@ ActiveRecord::Schema.define(version: 2021_01_22_085826) do
     t.integer "user_id"
     t.integer "test_id"
     t.integer "question_id"
-    t.integer "correct_questions", default: 0
+    t.integer "correct_answers", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "question_number", default: 1
     t.index ["question_id"], name: "index_test_passages_on_question_id"
     t.index ["test_id"], name: "index_test_passages_on_test_id"
     t.index ["user_id"], name: "index_test_passages_on_user_id"
@@ -75,6 +74,7 @@ ActiveRecord::Schema.define(version: 2021_01_22_085826) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "answers", "questions"
