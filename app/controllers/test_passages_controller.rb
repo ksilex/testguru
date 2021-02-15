dce81e1f99ce03b9cc48bd8a835b18468127e3d4
ksilex <negative074@gmail.com>
2021-02-15 16:46:48 +0300

    fixed redirect

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -9,9 +9,9 @@ class TestPassagesController < ApplicationController
 
   def update
     @test_passage.accept!(params[:answer_ids])
-    if @test_passage.completed?
+    if @test_passage.completed? || (@test_passage.test.timer? && @test_passage.times_up?)
       @test_passage.check_for_award if @test_passage.success?
-      redirect_to result_test_passage_path(@test_passage) if @test_passage.test.timer? && @test_passage.times_up?
+      redirect_to result_test_passage_path(@test_passage)
     else
     render :show
     end
